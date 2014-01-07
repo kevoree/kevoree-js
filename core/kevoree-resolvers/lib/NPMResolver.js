7b1b2b504ef378b45fd4f8324cdbb055a66682c8
Maxime Tricoire <max.tricoire@gmail.com>
2014-01-07 15:42:19 +0100

    NPMResolver now try to load module from current directory prior to remote install from npm registry (this allows npm run kevoree to work without publishing your module)

--- core/kevoree-resolvers/lib/NPMResolver.js
+++ core/kevoree-resolvers/lib/NPMResolver.js
@@ -10,6 +10,7 @@ var NPMResolver = Resolver.extend({
   construct: function (modulesPath, logger) {
     this.modulesPath = modulesPath || '';
     this.log = logger || new KevoreeLogger(this.toString());
+    this.log.debug(this.toString(), 'modulesPath= '+this.modulesPath);
   },
 
   resolve: function (deployUnit, forceInstall, callback) {
@@ -21,43 +22,129 @@ var NPMResolver = Resolver.extend({
 
     var packageName = deployUnit.name,
         module      = deployUnit.name + ((deployUnit.version.length > 0) ? '@'+deployUnit.version: ''),
-        loader      = new kevoree.loader.JSONModelLoader();
-
+        loader      = new kevoree.loader.JSONModelLoader(),
+        pkgPath     = path.resolve(this.modulesPath, 'node_modules', packageName); // default npm module location
+    
     function doResolve() {
-      var KClass = require(path.resolve(this.modulesPath, 'node_modules', packageName));
-      var jsonModel = require(path.resolve(this.modulesPath, 'node_modules', packageName, 'kevlib.json'));
-      return callback(null, KClass, loader.loadModelFromString(JSON.stringify(jsonModel)).get(0));
-    }
-
-    try {
-      if (forceInstall == true) {
-        // trigger FORCE_INSTALL error so installation is forced
-        var e = new Error();
-        e.code = 'FORCE_INSTALL';
-        throw e;
+      var KClass = require(pkgPath);
+      var jsonModel = require(path.resolve(pkgPath, 'kevlib.json'));
+      try {
+        var model = loader.loadModelFromString(JSON.stringify(jsonModel)).get(0);
+        return callback(null, KClass, model);
+      } catch (err) {
+        // something went wrong while loading model :/
+        return callback(err);
       }
-      doResolve.bind(this)();
-
-    } catch (err) {
-      if (err.code && err.code === "MODULE_NOT_FOUND" || err.code === "FORCE_INSTALL") {
-        this.log.info(this.toString(), "DeployUnit ("+module+") is not installed yet: downloading & installing it...");
-        npm.load({}, function (err) {
-          if (err) {
-            return callback(new Error('Unable to load npm module'));
-          }
-
-          // load success
-          npm.commands.install(this.modulesPath, [module], function installCallback(err) {
-            if (err) {
-              this.log.error(this.toString(), 'npm failed to install package \''+ module +'\'');
-              return callback(new Error("Bootstrap failure"));
-            }
-
-            doResolve.bind(this)();
-          }.bind(this));
-        }.bind(this));
-      } else throw err;
     }
+    
+    function resolveFromRegistry(installPath) {
+      npmInstall(installPath, module, function (err) {
+        if (err) {
+          this.log.error(this.toString(), 'npm failed to install package \''+ module +'\'');
+          return callback(new Error("Bootstrap failure"));
+        }
+        // npm install succeed so library should be installed as an npm module: go resolve
+        doResolve();
+      });
+    }
+    
+    // first of all: is "forceInstall" flag set to true ?
+    if (forceInstall == true) {
+      // user wants to reinstall library
+      
+    } else {
+      // we do not need to reinstall the library if it has already been installed
+      // lets check if it has been installed
+      if (isAvailable(pkgPath)) {
+        // library is already installed: great! go callback
+        doResolve();
+      } else {
+        // library is not installed as an npm module
+        // lets try to check if the current directory contains the library
+        // so that we can install it with the local content
+        try {
+          var pkg = require(path.resolve('.', 'package.json'));
+          if (pkg.name === packageName) {
+            // current directory contains the library
+            npmInstall(this.modulesPath, path.resolve('.'), function (err) {
+              if (err) {
+                this.log.error(this.toString(), 'npm failed to install package \''+ path.resolve('.') +'\'');
+                return callback(new Error("Bootstrap failure"));
+              }
+              // library should now be installed as an npm module
+              doResolve();
+            });
+          } else {
+            // well unable to find module locally, lets try to resolve it from npm registry
+            resolveFromRegistry(this.modulesPath);
+          }
+        } catch (err) {
+          // unable to require current directory package.json, lets try to resolve module from npm registry
+          resolveFromRegistry(this.modulesPath);
+        }
+      }
+    }
+
+//    function doResolve(someLib) {
+//      var KClass = require(someLib);
+//      var jsonModel = require(path.resolve(someLib, 'kevlib.json'));
+//      return callback(null, KClass, loader.loadModelFromString(JSON.stringify(jsonModel)).get(0));
+//    }
+//
+//    try {
+//      if (forceInstall == true) {
+//        // trigger FORCE_INSTALL error so installation is forced
+//        var e = new Error();
+//        e.code = 'FORCE_INSTALL';
+//        throw e;
+//      }
+//      doResolve.bind(this)(pkgPath);
+//
+//    } catch (err) {
+//      if (err.code === 'MODULE_NOT_FOUND' || err.code === 'FORCE_INSTALL') {
+//        try {
+//          // for non-published packages this is useful because it tries
+//          // to find the module in the current directory
+//          this.log.info(this.toString(), "DeployUnit ("+module+") is not installed yet: trying local install...");
+//          npm.load({}, function (err) {
+//            if (err) {
+//              return callback(new Error('Unable to load npm module'));
+//            }
+//
+//            // load success
+//            npm.commands.install(this.modulesPath, [path.resolve('..', packageName)], function (err) {
+//              if (err) {
+//                this.log.error(this.toString(), 'npm failed to install package \''+ module +'\'');
+//                return callback(new Error("Bootstrap failure"));
+//              }
+//
+//              doResolve.bind(this)(module);
+//            }.bind(this));
+//          }.bind(this));
+//          
+//        } catch (err) {
+//          if (err.code === 'MODULE_NOT_FOUND' || err.code === 'FORCE_INSTALL') {
+//            // try to download it from npm registry
+//            this.log.info(this.toString(), "DeployUnit ("+module+") is not installed yet: downloading & installing it...");
+//            npm.load({}, function (err) {
+//              if (err) {
+//                return callback(new Error('Unable to load npm module'));
+//              }
+//
+//              // load success
+//              npm.commands.install(this.modulesPath, [module], function (err) {
+//                if (err) {
+//                  this.log.error(this.toString(), 'npm failed to install package \''+ module +'\'');
+//                  return callback(new Error("Bootstrap failure"));
+//                }
+//
+//                doResolve.bind(this)(module);
+//              }.bind(this));
+//            }.bind(this));
+//          } else throw err;
+//        }
+//      } else throw err;
+//    }
   },
 
   uninstall: function (deployUnit, callback) {
@@ -70,7 +157,7 @@ var NPMResolver = Resolver.extend({
       var module = deployUnit.name + ((deployUnit.version.length > 0) ? '@'+deployUnit.version: '');
 
       // load success
-      npm.commands.uninstall(this.modulesPath, [module], function uninstallCallback(er) {
+      npm.commands.uninstall(this.modulesPath, [module], function (er) {
         if (er) {
           // failed to load package:version
           return callback(new Error('NPMResolver failed to uninstall '+module));
@@ -82,5 +169,25 @@ var NPMResolver = Resolver.extend({
     }.bind(this));
   }
 });
+
+function isAvailable(modulePath) {
+  try {
+    require.resolve(modulePath);
+    return true;
+  } catch (err) {}
+  return false;
+}
+
+function npmInstall(installPath, module, callback) {
+  npm.load({}, function (err) {
+    if (err) return callback(err);
+    
+    npm.commands.install(installPath, [module], function (err) {
+      if (err) return callback(err);
+      
+      return callback();
+    })
+  });
+}
 
 module.exports = NPMResolver;
--- tools/kevoree-kevscript/lib/statements/typeDef.js
+++ tools/kevoree-kevscript/lib/statements/typeDef.js
@@ -1,9 +1,28 @@
+var path = require('path');
+
 module.exports = function (model, statements, stmt, opts, cb) {
   var name = statements[stmt.children[0].type](model, statements, stmt.children[0], opts, cb);
 
   if (stmt.children[1]) {
+    // name & version specified
     return name + '/' + statements[stmt.children[1].type](model, statements, stmt.children[1], opts, cb);
   } else {
-    return name;
+    // try to find version on my own
+    // XXX something like this shouldn't be done here but rather in findTypeDefinitionByID
+    var tDefs = model.typeDefinitions.iterator();
+    var version = null;
+    while (tDefs.hasNext()) {
+      var tDef = tDefs.next();
+      console.log('TDEF > ',tDef.name);
+      if (tDef.name === name) {
+        version = tDef.version;
+        break;
+      }
+    }
+    if (version) {
+      return name + '/' + version;   
+    } else {
+      return name;
+    }
   }
 }
