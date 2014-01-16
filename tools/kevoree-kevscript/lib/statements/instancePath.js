d8e18cd6c5ad1566b9fa8e18a0c00ebada824c6e
Maxime Tricoire <max.tricoire@gmail.com>
2014-01-16 15:38:58 +0100

    bugfix: set statement kevoree-kevscript + v0.0.3

--- tools/kevoree-kevscript/lib/statements/instancePath.js
+++ tools/kevoree-kevscript/lib/statements/instancePath.js
@@ -5,6 +5,23 @@ module.exports = function (model, statements, stmt, opts, cb) {
   }
 
   return {
+    /**
+     * 'a.b.c' with expect(2, 4, function (err, one, two, three, four) {
+     *   if (err) {
+     *     // something went wrong with instancePath.expect parsing
+     *   }
+     *   
+     *   // when parsing 'a.b.c' with expect(2, 4, ...) you get:
+     *   // one   === null
+     *   // two   === 'a'
+     *   // three === 'b'
+     *   // four  === 'c'
+     * }
+     * @param min minimum expected values in instancePath (ex: a.b.c with "min" = 4 will return an error in callback)
+     * @param max maximum expected values in instancePath (same as minimum but for maximum :D )
+     * @param callback function (err, [arg0, arg1, ...])
+     * @returns {*}
+     */
     expect: function (min, max, callback) {
       if (instancePath.length > max || instancePath.length < min) {
         return callback(new Error('InstancePath does not match requirements (path: '+instancePath.join('.')+', length: '+instancePath.length+', min: '+min+', max: '+max+')'));
