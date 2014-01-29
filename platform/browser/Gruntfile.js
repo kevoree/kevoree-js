b6af867f20fedd308608e8b15db9f95db4f76244
Maxime Tricoire <max.tricoire@gmail.com>
2014-01-29 10:13:27 +0100

    bugfix: browser platform resolve

--- platform/browser/Gruntfile.js
+++ platform/browser/Gruntfile.js
@@ -4,41 +4,41 @@ var browserify = require('browserify');
 
 module.exports = function (grunt) {
 
-  // Project configuration.
-  grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
-    clean: {
-      dist: ['client/dist/'],
-      libraries: ['client/dist/public/libraries/']
-    },
-    copyto: {
-      main: {
-        files: [
-          {
-            cwd: 'client/site/',
-            src: [ '**/*' ],
-            dest: 'client/dist/'
-          }
-        ]
-      }
-    },
-    browserify: {
-      dist: {
-        files: {
-          'client/dist/public/js/<%= pkg.name %>.js': ['client/<%= pkg.name %>.js']
+    // Project configuration.
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        clean: {
+            dist: ['client/dist/'],
+            libraries: ['client/dist/public/libraries/']
         },
-        options: {
-          alias: [ path.resolve('node_modules', 'kevoree-library')+':kevoree-library' ]
-        }
-      },
-      kotlin: {
-        files: {
-          'client/dist/public/js/kevoree-kotlin.js': []
+        copyto: {
+            main: {
+                files: [
+                    {
+                        cwd: 'client/site/',
+                        src: [ '**/*' ],
+                        dest: 'client/dist/'
+                    }
+                ]
+            }
         },
-        options: {
-          alias: [ path.resolve('node_modules', 'kevoree-kotlin')+':kevoree-kotlin' ]
-        }
-      }
+        browserify: {
+            dist: {
+                files: {
+                    'client/dist/public/js/<%= pkg.name %>.js': ['client/<%= pkg.name %>.js']
+                },
+                options: {
+                    alias: [ path.resolve('node_modules', 'kevoree-library')+':kevoree-library' ]
+                }
+            },
+            kotlin: {
+                files: {
+                    'client/dist/public/js/kevoree-kotlin.js': []
+                },
+                options: {
+                    alias: [ path.resolve('node_modules', 'kevoree-kotlin')+':kevoree-kotlin' ]
+                }
+            }
 //      kevoree: {
 //        files: {
 //          'client/dist/public/js/kevoree-library.js': []
@@ -49,27 +49,28 @@ module.exports = function (grunt) {
 //          ]
 //        }
 //      }
-    },
-    express: {
-      custom: {
-        options: {
-          hostname: 'localhost',
-          port: 42042,
-          server: path.resolve('./server/app')
+        },
+        express: {
+            custom: {
+                options: {
+                    hostname: 'localhost',
+                    port: 42042,
+                    server: path.resolve('./server/app')
+                }
+            }
         }
-      }
-    }
-  });
+    });
 
-  grunt.registerTask('libfolder', 'Creates browser client libraries folder', function () {
-    grunt.file.mkdir('client/dist/public/libraries');
-  });
+    grunt.registerTask('libfolder', 'Creates browser client libraries folder', function () {
+        grunt.file.mkdir('client/dist/public/libraries');
+    });
 
-  grunt.loadNpmTasks('grunt-contrib-clean');
-  grunt.loadNpmTasks('grunt-copy-to');
-  grunt.loadNpmTasks('grunt-browserify');
-  grunt.loadNpmTasks('grunt-express');
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-copy-to');
+    grunt.loadNpmTasks('grunt-browserify');
+    grunt.loadNpmTasks('grunt-express');
 
-  grunt.registerTask('client', ['clean:dist', 'copyto:main', 'clean:libraries', 'browserify', 'libfolder']);
-  grunt.registerTask('server', ['express', 'express-keepalive']);
+    grunt.registerTask('client', ['clean:dist', 'copyto:main', 'clean:libraries', 'browserify', 'libfolder']);
+    grunt.registerTask('server', ['express', 'express-keepalive']);
+    grunt.registerTask('default', ['client', 'server']);
 };
