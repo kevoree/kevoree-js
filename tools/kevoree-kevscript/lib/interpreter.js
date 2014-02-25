109add7cbc24322982acf02487f0757f6e9b2690
Maxime Tricoire <max.tricoire@gmail.com>
2014-02-25 15:28:55 +0100

    kevoree-kevscript: handles realString, escaped & addRepo

--- tools/kevoree-kevscript/lib/interpreter.js
+++ tools/kevoree-kevscript/lib/interpreter.js
@@ -7,27 +7,29 @@ var cloner  = new kevoree.cloner.DefaultModelCloner();
 
 // retrieve statements processors
 var statements = {
-  addRepo:      require('./statements/addRepo'),
-  include:      require('./statements/include'),
-  add:          require('./statements/add'),
-  move:         require('./statements/move'),
-  attach:       require('./statements/attach'),
-  addBinding:   require('./statements/addBinding'),
-  delBinding:   require('./statements/delBinding'),
-  set:          require('./statements/set'),
-  network:      require('./statements/network'),
-  remove:       require('./statements/remove'),
-  detach:       require('./statements/detach'),
-  typeDef:      require('./statements/typeDef'),
-  nameList:     require('./statements/nameList'),
-  instancePath: require('./statements/instancePath'),
-  namespace:    require('./statements/namespace'),
-  wildcard:     require('./statements/wildcard'),
-  string:       require('./statements/string'),
-  string2:      require('./statements/string2'),
-  repoString:   require('./statements/repoString'),
-  version:      require('./statements/version'),
-  anything:     require('./statements/anything')
+    addRepo:      require('./statements/addRepo'),
+    include:      require('./statements/include'),
+    add:          require('./statements/add'),
+    move:         require('./statements/move'),
+    attach:       require('./statements/attach'),
+    addBinding:   require('./statements/addBinding'),
+    delBinding:   require('./statements/delBinding'),
+    set:          require('./statements/set'),
+    network:      require('./statements/network'),
+    remove:       require('./statements/remove'),
+    detach:       require('./statements/detach'),
+    typeDef:      require('./statements/typeDef'),
+    nameList:     require('./statements/nameList'),
+    instancePath: require('./statements/instancePath'),
+    namespace:    require('./statements/namespace'),
+    wildcard:     require('./statements/wildcard'),
+    string:       require('./statements/string'),
+    string2:      require('./statements/string2'),
+    repoString:   require('./statements/repoString'),
+    version:      require('./statements/version'),
+    anything:     require('./statements/anything'),
+    realString:   require('./statements/realString'),
+    escaped:      require('./statements/escaped')
 };
 
 /**
@@ -38,36 +40,36 @@ var statements = {
  * @param callback
  */
 var interpreter = function interpreter(ast, ctxModel, resolvers, callback) {
-  // output model
-  var model = null;
-  // if we have a context model, clone it and use it has a base
-  if (ctxModel) model = cloner.clone(ctxModel, false);
-  // otherwise start from a brand new model
-  else model = factory.createContainerRoot();
+    // output model
+    var model = null;
+    // if we have a context model, clone it and use it has a base
+    if (ctxModel) model = cloner.clone(ctxModel, false);
+    // otherwise start from a brand new model
+    else model = factory.createContainerRoot();
 
-  var options = {
-    resolvers: resolvers,
-    namespaces: {}
-  };
+    var options = {
+        resolvers: resolvers,
+        namespaces: {}
+    };
 
-  // process statements
-  var tasks = [];
-  for (var i in ast.children) {
-    for (var j in ast.children[i].children) {
-      (function (stmt) {
-        tasks.push(function (cb) {
-          statements[stmt.type](model, statements, stmt, options, cb);
-        });
-      })(ast.children[i].children[j]);
+    // process statements
+    var tasks = [];
+    for (var i in ast.children) {
+        for (var j in ast.children[i].children) {
+            (function (stmt) {
+                tasks.push(function (cb) {
+                    statements[stmt.type](model, statements, stmt, options, cb);
+                });
+            })(ast.children[i].children[j]);
+        }
     }
-  }
 
-  // execute tasks
-  async.series(tasks, function (err) {
-    if (err) return callback(err);
+    // execute tasks
+    async.series(tasks, function (err) {
+        if (err) return callback(err);
 
-    return callback(null, model);
-  });
+        return callback(null, model);
+    });
 }
 
 module.exports = interpreter;
--- tools/kevoree-kevscript/lib/statements/addRepo.js
+++ tools/kevoree-kevscript/lib/statements/addRepo.js
@@ -1,3 +1,10 @@
+var kevoree = require('kevoree-library').org.kevoree;
+var factory = new kevoree.impl.DefaultKevoreeFactory();
+
 module.exports = function (model, statements, stmt, opts, cb) {
-  console.log("Not implemented yet");
+    var url = statements[stmt.children[0].type](model, statements, stmt, opts, cb);
+    var repo = factory.createRepository();
+    repo.url = url;
+    model.addRepositories(repo);
+    cb();
 }
