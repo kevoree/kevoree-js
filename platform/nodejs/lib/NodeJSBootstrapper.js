ba6425b8d576d300d7188c478abdfba1320a56c7
Maxime Tricoire <max.tricoire@gmail.com>
2014-03-03 16:57:51 +0100

    remove FileResolver + update version

--- platform/nodejs/lib/NodeJSBootstrapper.js
+++ platform/nodejs/lib/NodeJSBootstrapper.js
@@ -1,7 +1,6 @@
 var Bootstrapper    = require('kevoree-commons').Bootstrapper,
-  NPMResolver     = require('kevoree-resolvers').NPMResolver,
-  FileResolver    = require('kevoree-resolvers').FileResolver,
-  path            = require('path');
+    NPMResolver     = require('kevoree-resolvers').NPMResolver,
+    path            = require('path');
 
 var FILE    = 'file',
     GIT     = 'git',
@@ -12,72 +11,71 @@ var FILE    = 'file',
  * @type {NPMBootstrapper}
  */
 module.exports = Bootstrapper.extend({
-  toString: "NodeJSBootstrapper",
+    toString: "NodeJSBootstrapper",
 
-  /**
-   *
-   */
-  construct: function (modulesPath, logger) {
-    this.log = logger;
+    /**
+     *
+     */
+    construct: function (modulesPath, logger) {
+        this.log = logger;
 
-    this.resolvers = {};
-    this.resolvers[NPM]  = new NPMResolver(modulesPath, logger);
-    this.resolvers[FILE] = new FileResolver(modulesPath, logger);
-  },
+        this.resolvers = {};
+        this.resolvers[NPM]  = new NPMResolver(modulesPath, logger);
+    },
 
-  /**
-   *
-   * @param deployUnit
-   * @param callback(Error, Clazz, ContainerRoot)
-   */
-  bootstrap: function (deployUnit, forceInstall, callback) {
-    if (typeof(callback) == 'undefined') {
-      // "forceInstall" parameter is not specified (optional)
-      callback = forceInstall;
-      forceInstall = false;
+    /**
+     *
+     * @param deployUnit
+     * @param callback(Error, Clazz, ContainerRoot)
+     */
+    bootstrap: function (deployUnit, forceInstall, callback) {
+        if (typeof(callback) == 'undefined') {
+            // "forceInstall" parameter is not specified (optional)
+            callback = forceInstall;
+            forceInstall = false;
+        }
+
+        // --- Resolvers callback
+        var bootstrapper = this;
+        this.resolver('resolve', deployUnit, forceInstall, function (err, EntityClass, model) {
+            if (err) {
+                bootstrapper.log.error(err.message);
+                callback(new Error("'"+deployUnit.name+"' bootstrap failed!"));
+                return;
+            }
+
+            // install success
+            callback(null, EntityClass, model);
+            return;
+        });
+    },
+
+    uninstall: function (deployUnit, callback) {
+        var bootstrapper = this;
+        this.resolver('uninstall', deployUnit, function (err) {
+            if (err) {
+                bootstrapper.log.error(err.message);
+                callback(new Error("'"+deployUnit.name+"' uninstall failed!"));
+                return;
+            }
+
+            // uninstall success
+            callback(null);
+            return;
+        });
+    },
+
+    resolver: function (action, deployUnit, forceInstall, callback) {
+        var url = deployUnit ? (deployUnit.url || '') : '';
+
+        if (url.startsWith(FILE)) {
+            this.log.warn("File resolver not implemented yet");
+
+        } else if (url.startsWith(GIT)) {
+            this.log.warn("Git resolver not implemented yet");
+
+        } else {
+            this.resolvers[NPM][action](deployUnit, forceInstall, callback);
+        }
     }
-
-    // --- Resolvers callback
-    var bootstrapper = this;
-    this.resolver('resolve', deployUnit, forceInstall, function (err, EntityClass, model) {
-      if (err) {
-        bootstrapper.log.error(err.message);
-        callback(new Error("'"+deployUnit.name+"' bootstrap failed!"));
-        return;
-      }
-
-      // install success
-      callback(null, EntityClass, model);
-      return;
-    });
-  },
-
-  uninstall: function (deployUnit, callback) {
-    var bootstrapper = this;
-    this.resolver('uninstall', deployUnit, function (err) {
-      if (err) {
-        bootstrapper.log.error(err.message);
-        callback(new Error("'"+deployUnit.name+"' uninstall failed!"));
-        return;
-      }
-
-      // uninstall success
-      callback(null);
-      return;
-    });
-  },
-
-  resolver: function (action, deployUnit, forceInstall, callback) {
-    var url = deployUnit ? (deployUnit.url || '') : '';
-
-    if (url.startsWith(FILE)) {
-      this.resolvers[FILE][action](deployUnit, forceInstall, callback);
-
-    } else if (url.startsWith(GIT)) {
-      this.log.warn("Git resolver not implemented yet");
-
-    } else {
-      this.resolvers[NPM][action](deployUnit, forceInstall, callback);
-    }
-  }
 });
