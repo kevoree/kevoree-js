0d8e679d9d4d52729db47aa2ef38e649b79b1645
Maxime Tricoire <max.tricoire@gmail.com>
2014-03-04 15:09:16 +0100

    grunt-kevoree 0.0.1

---
+++ tools/grunt-kevoree/Gruntfile.js
@@ -0,0 +1,56 @@
+/*
+ * grunt-kevoree
+ * https://github.com/kevoree/kevoree-js
+ *
+ * Copyright (c) 2014 Maxime Tricoire
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+
+module.exports = function(grunt) {
+
+    // Project configuration.
+    grunt.initConfig({
+        jshint: {
+            all: [
+                'Gruntfile.js',
+                'tasks/*.js'
+            ],
+            options: {
+                jshintrc: '.jshintrc'
+            }
+        },
+
+        // Before generating any new files, remove any previously-created files.
+        clean: {
+            tests: ['tmp']
+        },
+
+        // Configuration to be run (and then tested).
+        kevoree: {
+            run: {
+                options: {
+                    node: 'node0',
+                    group: 'sync'
+                },
+                kevscript: 'test/kevs/main.kevs'
+            }
+        }
+    });
+
+    // Actually load this plugin's task(s).
+    grunt.loadTasks('tasks');
+
+    // These plugins provide necessary tasks.
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-clean');
+
+    // Whenever the "test" task is run, first clean the "tmp" dir, then run this
+    // plugin's task(s), then test the result.
+    grunt.registerTask('test', ['clean', 'kevoree']);
+
+    // By default, lint and run all tests.
+    grunt.registerTask('default', ['jshint', 'test']);
+
+};
---
+++ tools/grunt-kevoree/tasks/kevoree.js
@@ -0,0 +1,58 @@
+/*
+ * grunt-kevoree
+ * https://github.com/kevoree/kevoree-js
+ *
+ * Copyright (c) 2014 Maxime Tricoire
+ * Licensed under the LGPL-3.0 license.
+ */
+
+'use strict';
+
+var Kevoree         = require('kevoree-nodejs-runtime'),
+    KevoreeLogger   = require('kevoree-commons').KevoreeLogger,
+    NPMResolver     = require('kevoree-resolvers').NPMResolver,
+    KevScript       = require('kevoree-kevscript'),
+    path            = require('path');
+
+module.exports = function(grunt) {
+
+    var logger  = new KevoreeLogger('KevoreeRuntime');
+
+    grunt.registerMultiTask('kevoree', 'Automatically runs kevoree runtime (works like mvn kev:run plugin)', function (nodeName) {
+        var done = this.async();
+
+        // Merge task-specific and/or target-specific options with these defaults.
+        var options = this.options({
+            node: 'node0',
+            group: 'sync',
+            modulesPath: path.resolve('node_modules/grunt-kevoree')
+        });
+        if (nodeName) {
+            // if an argument is given to the task, then consider it is the node name to start the platform on (override)
+            options.node = nodeName;
+        }
+
+        var runtime     = new Kevoree(options.modulesPath),
+            npmResolver = new NPMResolver(options.modulesPath, logger),
+            kevsEngine  = new KevScript({ resolvers: { npm: npmResolver } });
+
+        runtime.on('started', function () {
+            var kevs = grunt.file.read(this.data.kevscript);
+            kevsEngine.parse(kevs, function (err, model) {
+                if (err) {
+                    grunt.log.error(err.message);
+                    done();
+                }
+
+                runtime.deploy(model);
+            });
+        }.bind(this));
+
+        runtime.on('deployed', function (model) {
+            grunt.log.ok('grunt-kevoree: model from '+this.data.kevscript+' deployed successfully :)');
+        }.bind(this));
+
+        runtime.start(options.node, options.group);
+    });
+
+};
