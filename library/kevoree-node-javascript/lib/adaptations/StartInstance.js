43b357c58f860ffabf7b65bc505ee2c6f3d6f5b6
Maxime Tricoire <max.tricoire@gmail.com>
2014-03-06 14:10:18 +0100

    use attribute defaultValues if none set before startInstance

--- library/kevoree-node-javascript/lib/adaptations/StartInstance.js
+++ library/kevoree-node-javascript/lib/adaptations/StartInstance.js
@@ -1,34 +1,45 @@
 var AdaptationPrimitive = require('./AdaptationPrimitive'),
-  StopInstance        = require('./StopInstance');
+    StopInstance        = require('./StopInstance');
 
 module.exports = AdaptationPrimitive.extend({
-  toString: 'StartInstance',
+    toString: 'StartInstance',
 
-  execute: function (_super, callback) {
-    _super.call(this, callback);
+    execute: function (_super, callback) {
+        _super.call(this, callback);
 
-    if (this.modelElement.name != this.node.getName() && this.isRelatedToPlatform(this.modelElement)) {
-      var instance = this.mapper.getObject(this.modelElement.path());
+        if (this.modelElement.name != this.node.getName() && this.isRelatedToPlatform(this.modelElement)) {
+            var instance = this.mapper.getObject(this.modelElement.path());
 
-      if (instance != undefined && instance != null) {
-        instance.start();
-        this.log.debug(this.toString(), 'job done for '+instance.getName()+'@'+this.node.getName());
+            if (instance != undefined && instance != null) {
+                // check dictionary value and give default values if none set
+                var dicType = this.modelElement.typeDefinition.dictionaryType;
+                if (dicType) {
+                    var attrs = dicType.attributes.iterator();
+                    while (attrs.hasNext()) {
+                        var attr = attrs.next();
+                        var val = instance.dictionary.getValue(attr.name);
+                        if (!val) instance.dictionary.setEntry(attr.name, attr.defaultValue);
+                    }
+                }
+
+                instance.start();
+                this.log.debug(this.toString(), 'job done for '+instance.getName()+'@'+this.node.getName());
+                return callback();
+
+            } else {
+                return callback(new Error(this.toString()+" error: unable to start instance "+this.modelElement.name));
+            }
+        }
+
         return callback();
+    },
 
-      } else {
-        return callback(new Error(this.toString()+" error: unable to start instance "+this.modelElement.name));
-      }
+    undo: function (_super, callback) {
+        _super.call(this, callback);
+
+        var cmd = new StopInstance(this.node, this.mapper, this.adaptModel, this.modelElement);
+        cmd.execute(callback);
+
+        return;
     }
-
-    return callback();
-  },
-
-  undo: function (_super, callback) {
-    _super.call(this, callback);
-
-    var cmd = new StopInstance(this.node, this.mapper, this.adaptModel, this.modelElement);
-    cmd.execute(callback);
-
-    return;
-  }
 });
