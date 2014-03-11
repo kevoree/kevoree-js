5480c0bf4fb298563e04403df1e78b2fcd119974
Maxime Tricoire <max.tricoire@gmail.com>
2014-03-11 16:49:57 +0100

    update versions

--- Gruntfile.js
+++ Gruntfile.js
@@ -17,7 +17,8 @@ module.exports = function(grunt) {
         deps_manager: {
             options: {
                 version: '<%= pkg.version %>',
-                pattern: 'kevoree-.*'
+                pattern: 'kevoree-.*',
+                ignore: 'kevoree-web-editor-client' // kevoree editor static client isn't in this project repository
             },
             src: packages
         },
--- library/kevoree-comp-fakeconsole/Gruntfile.js
+++ library/kevoree-comp-fakeconsole/Gruntfile.js
@@ -1,17 +1,23 @@
 module.exports = function (grunt) {
 
-  grunt.initConfig({
-    jade: {
-      node: {
-        files: {
-          'generated-ui/': ['ui/*.jade']
+    grunt.initConfig({
+        jade: {
+            node: {
+                files: {
+                    'generated-ui/': ['ui/*.jade']
+                },
+                options: {
+                    wrap: 'node'
+                }
+            }
         },
-        options: {
-          wrap: 'node'
+        kevoree: {
+            run: {
+                kevscript: 'kevs/main.kevs'
+            }
         }
-      }
-    }
-  });
+    });
 
-  grunt.loadNpmTasks('grunt-jade');
+    grunt.loadNpmTasks('grunt-jade');
+    grunt.loadNpmTasks('grunt-kevoree');
 }
