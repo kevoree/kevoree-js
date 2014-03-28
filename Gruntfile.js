ddb12c12054b6c7216edb029e58b63e8252326c0
Maxime Tricoire <max.tricoire@gmail.com>
2014-03-28 10:51:39 +0100

    new release v0.2.0

--- Gruntfile.js
+++ Gruntfile.js
@@ -29,7 +29,7 @@ module.exports = function(grunt) {
                     'org.kevoree.model.js'
                 ]
             },
-            src: [ '**/*' ]
+            src: [ '**' ]
         }
     });
 
