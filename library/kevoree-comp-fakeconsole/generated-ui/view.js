8967f278cd8d5f599adbbcfa855b8467841f89ce
Maxime Tricoire <max.tricoire@gmail.com>
2014-04-07 13:52:07 +0200

    update comp

--- library/kevoree-comp-fakeconsole/generated-ui/view.js
+++ library/kevoree-comp-fakeconsole/generated-ui/view.js
@@ -6,7 +6,7 @@ attrs = attrs || jade.attrs; escape = escape || jade.escape; rethrow = rethrow |
 var buf = [];
 with (locals || {}) {
 var interp;
-buf.push('<style>@import url(\'//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css\')\n</style><div class="container-fluid"><div class="row"><div class="input-group"><input id="msg" type="text" class="form-control"/><span class="input-group-btn"><button id="send" type="button" class="btn btn-primary">' + escape((interp = btn) == null ? '' : interp) + '</button></span></div></div><div class="row"><dl id="msg-list" class="dl-horizontal col-xs-12"></dl></div></div>');
+buf.push('<style>@import url(\'./node_modules/kevoree-comp-fakeconsole/resources/css/bootstrap.min.css\')\n</style><div class="container-fluid"><div class="row"><div class="input-group"><input id="msg" type="text" class="form-control"/><span class="input-group-btn"><button id="send" type="button" class="btn btn-primary">' + escape((interp = btn) == null ? '' : interp) + '</button></span></div></div><div class="row"><dl id="msg-list" class="dl-horizontal col-xs-12"></dl></div></div>');
 }
 return buf.join("");
 };
