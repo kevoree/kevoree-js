512f23e6787f6a63564b9e578a1dd96b58772937
Maxime Tricoire <max.tricoire@gmail.com>
2014-04-07 11:38:00 +0200

    kevoree-commons (KevoreeLogger) now using 'chalk' instead of 'colors' as coloring module for console outputs

--- core/kevoree-commons/lib/KevoreeLogger.js
+++ core/kevoree-commons/lib/KevoreeLogger.js
@@ -1,64 +1,69 @@
 var Class  = require('pseudoclass'),
-    colors = require('colors');
+    chalk  = require('chalk');
+
+var chalkInfo  = chalk.grey,
+    chalkWarn  = chalk.grey.bgYellow,
+    chalkError = chalk.white.bgRed,
+    chalkDebug = chalk.cyan;
 
 var KevoreeLogger = Class({
-  toString: 'KevoreeLogger',
+    toString: 'KevoreeLogger',
 
-  construct: function (tag) {
-    this.tag = tag;
-  },
+    construct: function (tag) {
+        this.tag = tag;
+    },
 
-  info: function (tag, msg) {
-    if (typeof(msg) == 'undefined') {
-      msg = tag;
-      tag = this.tag;
+    info: function (tag, msg) {
+        if (typeof(msg) === 'undefined') {
+            msg = tag;
+            tag = this.tag;
+        }
+        console.log(getTime()+'  '+chalkInfo('INFO')+'   '+processTag(tag)+'  '+chalk.grey(msg));
+    },
+
+    warn: function (tag, msg) {
+        if (typeof(msg) === 'undefined') {
+            msg = tag;
+            tag = this.tag;
+        }
+        console.warn(getTime()+'  '+chalkWarn('WARN')+'   '+processTag(tag)+'  '+chalk.yellow(msg));
+    },
+
+    error: function (tag, msg) {
+        if (typeof(msg) === 'undefined') {
+            msg = tag;
+            tag = this.tag;
+        }
+        console.error(getTime()+'  '+chalkError('ERROR')+'  '+processTag(tag)+'  '+chalk.red(msg));
+    },
+
+    debug: function (tag, msg) {
+        if (typeof(msg) === 'undefined') {
+            msg = tag;
+            tag = this.tag;
+        }
+        console.log(getTime()+'  '+chalkDebug('DEBUG ')+' '+processTag(tag)+'  '+chalk.cyan(msg));
     }
-    console.log(getTime()+'  '+'INFO'.grey+'   '+processTag(tag)+'  '+msg.grey);
-  },
-
-  warn: function (tag, msg) {
-    if (typeof(msg) == 'undefined') {
-      msg = tag;
-      tag = this.tag;
-    }
-    console.warn(getTime()+'  '+'WARN'.grey.yellowBG+'   '+processTag(tag)+'  '+msg.yellow);
-  },
-
-  error: function (tag, msg) {
-    if (typeof(msg) == 'undefined') {
-      msg = tag;
-      tag = this.tag;
-    }
-    console.error(getTime()+'  '+'ERROR'.white.redBG+'  '+processTag(tag)+'  '+msg.red);
-  },
-
-  debug: function (tag, msg) {
-    if (typeof(msg) == 'undefined') {
-      msg = tag;
-      tag = this.tag;
-    }
-    console.log(getTime()+'  '+'DEBUG '.cyan+' '+processTag(tag)+'  '+msg.cyan);
-  }
 });
 
 var processTag = function processTag(tag) {
-  if (tag.length >= 15) {
-    tag = tag.substr(0, 14)+'.';
-  } else {
-    var spaces = '';
-    for (var i=0; i < 15 - tag.length; i++) spaces += ' ';
-    tag += spaces;
-  }
+    if (tag.length >= 15) {
+        tag = tag.substr(0, 14)+'.';
+    } else {
+        var spaces = '';
+        for (var i=0; i < 15 - tag.length; i++) spaces += ' ';
+        tag += spaces;
+    }
 
-  return (tag+'').magenta;
+    return chalk.magenta(tag);
 }
 
 var getTime = function getTime() {
-  var time = new Date;
-  var hours = (time.getHours().toString().length == 1) ? '0'+time.getHours() : time.getHours();
-  var mins = (time.getMinutes().toString().length == 1) ? '0'+time.getMinutes() : time.getMinutes();
-  var secs = (time.getSeconds().toString().length == 1) ? '0'+time.getSeconds() : time.getSeconds();
-  return (hours+':'+mins+':'+secs).grey;
-}
+    var time = new Date();
+    var hours = (time.getHours().toString().length == 1) ? '0'+time.getHours() : time.getHours();
+    var mins = (time.getMinutes().toString().length == 1) ? '0'+time.getMinutes() : time.getMinutes();
+    var secs = (time.getSeconds().toString().length == 1) ? '0'+time.getSeconds() : time.getSeconds();
+    return chalk.grey(hours+':'+mins+':'+secs);
+};
 
 module.exports = KevoreeLogger;
