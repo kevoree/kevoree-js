92da6dc1d5505d7817e9e1ee11a6e7e2e39fb6f5
Maxime Tricoire <max.tricoire@gmail.com>
2014-04-07 17:40:45 +0200

    KevoreeLogger level improvement

--- core/kevoree-commons/lib/KevoreeLogger.js
+++ core/kevoree-commons/lib/KevoreeLogger.js
@@ -11,38 +11,51 @@ var KevoreeLogger = Class({
 
     construct: function (tag) {
         this.tag = tag;
+        this.level = 0; // all
     },
 
     info: function (tag, msg) {
-        if (typeof(msg) === 'undefined') {
-            msg = tag;
-            tag = this.tag;
+        if (this.level <= LEVEL_INFO) {
+            if (typeof(msg) === 'undefined') {
+                msg = tag;
+                tag = this.tag;
+            }
+            console.log(getTime()+'  '+chalkInfo('INFO')+'   '+processTag(tag)+'  '+chalk.grey(msg));
         }
-        console.log(getTime()+'  '+chalkInfo('INFO')+'   '+processTag(tag)+'  '+chalk.grey(msg));
     },
 
     warn: function (tag, msg) {
-        if (typeof(msg) === 'undefined') {
-            msg = tag;
-            tag = this.tag;
+        if (this.level <= LEVEL_WARN) {
+            if (typeof(msg) === 'undefined') {
+                msg = tag;
+                tag = this.tag;
+            }
+            console.warn(getTime()+'  '+chalkWarn('WARN')+'   '+processTag(tag)+'  '+chalk.yellow(msg));
         }
-        console.warn(getTime()+'  '+chalkWarn('WARN')+'   '+processTag(tag)+'  '+chalk.yellow(msg));
     },
 
     error: function (tag, msg) {
-        if (typeof(msg) === 'undefined') {
-            msg = tag;
-            tag = this.tag;
+        if (this.level <= LEVEL_ERROR) {
+            if (typeof(msg) === 'undefined') {
+                msg = tag;
+                tag = this.tag;
+            }
+            console.error(getTime() + '  ' + chalkError('ERROR') + '  ' + processTag(tag) + '  ' + chalk.red(msg));
         }
-        console.error(getTime()+'  '+chalkError('ERROR')+'  '+processTag(tag)+'  '+chalk.red(msg));
     },
 
     debug: function (tag, msg) {
-        if (typeof(msg) === 'undefined') {
-            msg = tag;
-            tag = this.tag;
+        if (this.level <= LEVEL_DEBUG) {
+            if (typeof(msg) === 'undefined') {
+                msg = tag;
+                tag = this.tag;
+            }
+            console.log(getTime()+'  '+chalkDebug('DEBUG ')+' '+processTag(tag)+'  '+chalk.cyan(msg));
         }
-        console.log(getTime()+'  '+chalkDebug('DEBUG ')+' '+processTag(tag)+'  '+chalk.cyan(msg));
+    },
+
+    setLevel: function (level) {
+        this.level = level;
     }
 });
 
@@ -56,7 +69,7 @@ var processTag = function processTag(tag) {
     }
 
     return chalk.magenta(tag);
-}
+};
 
 var getTime = function getTime() {
     var time = new Date();
@@ -65,5 +78,12 @@ var getTime = function getTime() {
     var secs = (time.getSeconds().toString().length == 1) ? '0'+time.getSeconds() : time.getSeconds();
     return chalk.grey(hours+':'+mins+':'+secs);
 };
+
+var LEVEL_ALL   = KevoreeLogger.ALL     = 0,
+    LEVEL_INFO  = KevoreeLogger.INFO    = 1,
+    LEVEL_DEBUG = KevoreeLogger.DEBUG   = 2,
+    LEVEL_WARN  = KevoreeLogger.WARN    = 3,
+    LEVEL_ERROR = KevoreeLogger.ERROR   = 4,
+    LEVEL_QUIET = KevoreeLogger.QUIET   = 5;
 
 module.exports = KevoreeLogger;
--- core/kevoree-commons/test/test-logger.js
+++ core/kevoree-commons/test/test-logger.js
@@ -1,12 +1,30 @@
 var Logger = require('./../lib/KevoreeLogger');
 
 var log = new Logger('MyTag');
-log.info('This is an info');
-log.debug('This is an debug');
-log.warn('This is an warn');
-log.error('This is an error');
 
-log.info('NewTag', 'This is an info');
-log.debug('VeryLongTagToMessWithLogger', 'This is an debug');
-log.warn('MMMMWWWWWMMMMWWWW', 'This is an warn');
-log.error('Potato With Space', 'This is an error');
+function doLog(level) {
+    log.setLevel(level);
+
+    log.info('This is an info');
+    log.debug('This is a debug');
+    log.warn('This is a warn');
+    log.error('This is a error');
+
+    log.info('NewTag', 'This is an info');
+    log.debug('VeryLongTagToMessWithLogger', 'This is a debug');
+    log.warn('MMMMWWWWWMMMMWWWW', 'This is a warn');
+    log.error('Potato With Space', 'This is a error');
+}
+
+console.log('============= Level Logger.ALL');
+doLog(Logger.ALL);
+console.log('============= Level Logger.INFO');
+doLog(Logger.INFO);
+console.log('============= Level Logger.DEBUG');
+doLog(Logger.DEBUG);
+console.log('============= Level Logger.WARN');
+doLog(Logger.WARN);
+console.log('============= Level Logger.ERROR');
+doLog(Logger.ERROR);
+console.log('============= Level Logger.QUIET');
+doLog(Logger.QUIET);
