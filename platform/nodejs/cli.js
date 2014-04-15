44d693abdce1eb736b7da6cda5cc31732a1bce34
Maxime Tricoire <max.tricoire@gmail.com>
2014-04-15 15:36:59 +0200

    update kevoree-nodejs-runtime to handle -p to specify where to install modules

--- platform/nodejs/cli.js
+++ platform/nodejs/cli.js
@@ -8,19 +8,22 @@ var config        = require('./config.json'),
     NPMResolver   = require('kevoree-resolvers').NPMResolver,
     KevScript     = require('kevoree-kevscript'),
     argv          = require('optimist')
-                        .usage('Usage: $0 [--nodeName node0 --groupName sync (--model path/to/your/model.json | --kevscript path/to/your/model.kevs)]')
+                        .usage('Usage: $0 [--nodeName node0 --groupName sync (--model path/to/your/model.json | --kevscript path/to/your/model.kevs) --modulesPath .]')
                         .alias('n', 'nodeName')
                         .alias('g', 'groupName')
                         .alias('m', 'model')
                         .alias('k', 'kevscript')
+                        .alias('p', 'modulesPath')
+                        .default('p', path.resolve('.'))
                         .describe('nodeName', 'The name of the node platform you want to bootstrap on')
                         .describe('groupName', 'The group name to which your node is gonna be connected to')
                         .describe('model', 'A model to bootstrap on')
                         .describe('kevscript', 'A KevScript file to bootstrap on')
+                        .describe('modulesPath', 'Where to install resolved modules')
                         .argv;
 
 // TODO enable install dir path in command-line
-var kRuntime = new NodeJSRuntime(__dirname);
+var kRuntime = new NodeJSRuntime(argv.modulesPath);
 var loader   = new kevoree.loader.JSONModelLoader();
 var log      = new KevoreeLogger('NodeJSRuntime');
 
@@ -33,7 +36,7 @@ kRuntime.on('started', function () {
             if (err) throw err;
 
             var options = {
-                resolvers: { npm: new NPMResolver(__dirname, log) }
+                resolvers: { npm: new NPMResolver(argv.modulesPath, log) }
             };
             var kevs = new KevScript(options);
             kevs.parse(text, function (err, model) {
