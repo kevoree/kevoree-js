fc4a649702fece1e9775a8e41233a0146e6bfb7e
Maxime Tricoire <max.tricoire@gmail.com>
2014-05-06 16:15:37 +0200

    kevoree-model-sync: update to handle path too (bump 0.6.1)

--- core/kevoree-model-sync/lib/push.js
+++ core/kevoree-model-sync/lib/push.js
@@ -6,26 +6,28 @@ var serializer = new kevoree.serializer.JSONModelSerializer();
 var PUSH = 'push';
 
 module.exports = function (options, callback) {
-  if (arguments.length == 0 || typeof options === 'function') throw new Error('You must give an options parameter to push.js');
-  if (!options.model) throw new Error('You must give a model to options parameter');
-  
-  callback = callback || function () {};
-  options.host = options.host || '127.0.0.1';
-  options.port = options.port || '8000';
-  
-  var ws;
-  try {
-    var modelStr = serializer.serialize(options.model);
-    ws = new WebSocket('ws://'+options.host+':'+options.port);
-    ws.onopen = function () {
-      // when the connection with the server is established push model to it
-      ws.send(PUSH+'/'+modelStr);
-      return callback();
-    }
-  } catch (err) {
-    return callback(err);
+    if (arguments.length == 0 || typeof options === 'function') throw new Error('You must give an options parameter to push.js');
+    if (!options.model) throw new Error('You must give a model to options parameter');
 
-  } finally {
-    //if (ws) ws.close();
-  }
+    callback = callback || function () {};
+    options.host = options.host || '127.0.0.1';
+    options.port = options.port || '8000';
+    options.path = options.path || '/';
+
+    var ws;
+    try {
+        var modelStr = serializer.serialize(options.model);
+        if (options.path.substr(0, 1) !== '/') options.path = '/' + options.path;
+        ws = new WebSocket('ws://'+options.host+':'+options.port+options.path);
+        ws.onopen = function () {
+            // when the connection with the server is established push model to it
+            ws.send(PUSH+'/'+modelStr);
+            return callback();
+        }
+    } catch (err) {
+        return callback(err);
+
+    } finally {
+        //if (ws) ws.close();
+    }
 }
