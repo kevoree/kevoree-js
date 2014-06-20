f9d8e878464f478dba465e837830d8e6729eef0b
Maxime Tricoire <max.tricoire@gmail.com>
2014-06-20 10:48:32 +0200

    kevoree-kevscript: bugfix on Stop generation statements for model2kevs

--- tools/kevoree-kevscript/lib/elements/lifecycles.js
+++ tools/kevoree-kevscript/lib/elements/lifecycles.js
@@ -5,24 +5,25 @@ module.exports = function (model) {
     var str = '';
     var toStop = [];
 
-    function addElems(instances) {
+    function addElems(instances, host) {
         while (instances.hasNext()) {
             var instance = instances.next();
             if (!instance.started) {
                 // instance is stopped => add to toStop array
-                if (instance.host) {
-                    toStop.push(instance.host.name+'.'+instance.name);
+                if (host) {
+                    toStop.push(host.name+'.'+instance.name);
                 } else {
                     toStop.push(instance.name);
                 }
-                // if instance has components => check for their state too
-                if (instance.components) {
-                    addElems(instance.components.iterator());
-                }
-                // if instance has hosts => check for their state too
-                if (instance.hosts) {
-                    addElems(instance.hosts.iterator());
-                }
+            }
+
+            // if instance has components => check for their state too
+            if (instance.components) {
+                addElems(instance.components.iterator(), instance);
+            }
+            // if instance has hosts => check for their state too
+            if (instance.hosts) {
+                addElems(instance.hosts.iterator(), instance);
             }
         }
     }
