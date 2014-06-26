f4099ccaa4c3d7abdc8a938dfdb76de4cfa95bec
Maxime Tricoire <max.tricoire@gmail.com>
2014-06-26 15:49:22 +0200

    add kevoree-library-manager in kevoree-js/library in order to manager all std libs easily

---
+++ library/Gruntfile.js
@@ -0,0 +1,33 @@
+module.exports = function(grunt) {
+
+    var packages = [
+        '*/package.json'
+    ];
+
+    // Project configuration.
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('../package.json'),
+        version: {
+            src: packages
+        },
+        deps_manager: {
+            options: {
+                version: '^<%= pkg.version %>',
+                pattern: ['kevoree-.*']
+            },
+            src: packages
+        },
+        publish: {
+            options: {
+                ignore: [ 'node_modules' ]
+            },
+            src: [ '*' ]
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-version');
+    grunt.loadNpmTasks('grunt-publish');
+    grunt.loadNpmTasks('grunt-deps-manager');
+
+    grunt.registerTask('release', ['version', 'deps_manager', 'publish']);
+};
