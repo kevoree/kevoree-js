a8f2a7cd71d6a78258ad2171c7a6c652c5c08550
Maxime Tricoire <max.tricoire@gmail.com>
2014-07-09 13:59:19 +0200

    kevoree-entities: AdaptationPrimitive relocation

--- core/kevoree-entities/kevoree-entities.js
+++ core/kevoree-entities/kevoree-entities.js
@@ -2,5 +2,6 @@ exports.KevoreeEntity       = require('./lib/KevoreeEntity');
 exports.AbstractGroup       = require('./lib/AbstractGroup');
 exports.AbstractChannel     = require('./lib/AbstractChannel');
 exports.AbstractNode        = require('./lib/AbstractNode');
+exports.AdaptationPrimitive = require('./lib/AdaptationPrimitive');
 exports.AbstractComponent   = require('./lib/AbstractComponent');
 exports.Port                = require('./lib/Port');
