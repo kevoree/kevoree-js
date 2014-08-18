38efacee9a0bd740956a00e3259944c9d5d281a7
Maxime Tricoire <max.tricoire@gmail.com>
2014-08-18 15:21:45 +0200

    kevoree-group-remotews: 2.0.0-SNAPSHOT impl new kevoree-entities@^2.0.0 and new smart-socket@^0.2.0

--- library/kevoree-group-remotews/lib/RemoteWSGroup.js
+++ library/kevoree-group-remotews/lib/RemoteWSGroup.js
@@ -2,7 +2,7 @@ var AbstractGroup = require('kevoree-entities').AbstractGroup,
     SmartSocket   = require('smart-socket'),
     kevoree       = require('kevoree-library').org.kevoree;
 
-var TIMEOUT = 5000;
+var LOOP_TIMEOUT = 5000;
 
 /**
  * Kevoree group
@@ -11,102 +11,94 @@ var TIMEOUT = 5000;
 var RemoteWSGroup = AbstractGroup.extend({
     toString: 'RemoteWSGroup',
 
-    dic_host: {
-        optional: false,
-        update: function () {
-            this.stop();
-            this.start();
-        }
-    },
-
-    dic_port: {
-        optional: false,
-        update: function () {
-            this.stop();
-            this.start();
-        }
-    },
-
-    dic_path: {
-        optional: true,
-        update: function () {
-            this.stop();
-            this.start();
-        }
-    },
+    dic_host: { optional: false },
+    dic_port: { optional: false, datatype: 'number' },
+    dic_path: { optional: true },
 
     /**
-    * this method will be called by the Kevoree platform when your group has to start
-    */
-    start: function () {
-        this._super();
+     * this method will be called by the Kevoree platform when your group has to start
+     * @param done
+     */
+    start: function (done) {
+        this._super(function () {
+            var host = this.dictionary.getValue('host'),
+                port = this.dictionary.getValue('port'),
+                path = this.dictionary.getValue('path');
 
-        var host = this.dictionary.getValue('host'),
-            port = this.dictionary.getValue('port'),
-            path = this.dictionary.getValue('path');
-
-        if (isNaN(parseInt(port))) {
-            throw new Error(this.toString()+' error: attribute "port" is not a number ('+port+')');
-        }
-
-        var address = host + ':' + port + processPath(path);
-
-        this.ss = new SmartSocket({
-            addresses: [ address ],
-            timeout: TIMEOUT,
-            handlers: {
-                onopen: function (ws) {
-                    this.log.info(this.toString(), 'Connected to ws://'+address);
-                    ws.send(JSON.stringify({action: 'register', id: this.getPath()}));
-                }.bind(this),
-
-                onclose: function () {
-                    this.log.info(this.toString(), 'Connection lost with ws://'+address+' (will retry every '+TIMEOUT+'ms until reconnected)');
-                }.bind(this),
-
-                onmessage: function (ws, msg) {
-                    if (msg.type) msg = msg.data;
-
-                    try {
-                        var factory = new kevoree.factory.DefaultKevoreeFactory();
-                        msg = JSON.parse(msg);
-                        switch (msg.action) {
-                            case 'push':
-                                this.log.info(this.toString(), ws._socket.remoteAddress+":"+ws._socket.remotePort+" asked for a PUSH");
-
-                                var jsonLoader = factory.createJSONLoader();
-                                var model = jsonLoader.loadModelFromString(msg.model).get(0);
-                                this.kCore.deploy(model);
-                                break;
-
-                            case 'pull':
-                                this.log.info(this.toString(), ws._socket.remoteAddress+":"+ws._socket.remotePort+" asked for a PULL");
-
-                                var serializer = factory.createJSONSerializer();
-                                var strModel = serializer.serialize(this.kCore.getCurrentModel());
-                                ws.send(JSON.stringify({
-                                    action: 'pullAnswer',
-                                    id: msg.id,
-                                    model: strModel
-                                }));
-                                break;
-                        }
-                    } catch (err) {
-                        this.log.warn(this.toString(), '"'+this.getName()+'" unable to process incoming message ('+msg.toString()+')');
-                    }
-                }.bind(this)
+            if (isNaN(parseInt(port))) {
+                done(new Error(this.toString()+' error: attribute "port" is not a number ('+port+')'));
+                return;
             }
-        });
 
-        this.ss.start();
+            var address = host + ':' + port + processPath(path);
+
+            this.ss = new SmartSocket({
+                addresses: [ address ],
+                loopBreak: LOOP_TIMEOUT,
+                handlers: {
+                    onopen: function (ws) {
+                        this.log.info(this.toString(), 'Connected to ws://'+address);
+                        ws.send(JSON.stringify({action: 'register', id: this.getPath()}));
+                    }.bind(this),
+
+                    onclose: function () {
+                        this.log.info(this.toString(), 'Connection lost with ws://'+address+' (will retry every '+LOOP_TIMEOUT+'ms until reconnected)');
+                    }.bind(this),
+
+                    onerror: function (ws, err) {
+                        this.log.info(this.toString(), 'Connection problem with ws://'+address+' (will retry every '+LOOP_TIMEOUT+'ms until connected)');
+                    }.bind(this),
+
+                    onmessage: function (ws, msg) {
+                        if (msg.type) msg = msg.data;
+
+                        try {
+                            var factory = new kevoree.factory.DefaultKevoreeFactory();
+                            msg = JSON.parse(msg);
+                            switch (msg.action) {
+                                case 'push':
+                                    this.log.info(this.toString(), ws._socket.remoteAddress+":"+ws._socket.remotePort+" asked for a PUSH");
+
+                                    var jsonLoader = factory.createJSONLoader();
+                                    var model = jsonLoader.loadModelFromString(msg.model).get(0);
+                                    this.kCore.deploy(model);
+                                    break;
+
+                                case 'pull':
+                                    this.log.info(this.toString(), ws._socket.remoteAddress+":"+ws._socket.remotePort+" asked for a PULL");
+
+                                    var serializer = factory.createJSONSerializer();
+                                    var strModel = serializer.serialize(this.kCore.getCurrentModel());
+                                    ws.send(JSON.stringify({
+                                        action: 'pullAnswer',
+                                        id: msg.id,
+                                        model: strModel
+                                    }));
+                                    break;
+                            }
+                        } catch (err) {
+                            this.log.warn(this.toString(), '"'+this.getName()+'" unable to process incoming message ('+msg.toString()+')');
+                        }
+                    }.bind(this)
+                }
+            });
+
+            this.ss.start();
+
+            // don't wait for this to successfully connect
+            done();
+        }.bind(this));
     },
 
     /**
-    * this method will be called by the Kevoree platform when your group has to stop
-    */
-    stop: function () {
-        this._super();
-        this.ss.close(true);
+     * this method will be called by the Kevoree platform when your group has to stop
+     * @param done
+     */
+    stop: function (done) {
+        this._super(function () {
+            this.ss.close(true);
+            done();
+        }.bind(this));
     }
 });
 
