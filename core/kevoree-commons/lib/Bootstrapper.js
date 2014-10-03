113b91a45b3667eda8a515034cc4dfaede2ce7b6
Maxime Tricoire <max.tricoire@gmail.com>
2014-10-03 16:14:48 +0200

    kevoree-commons: v2.1.0 Bootstrapper now contains the bare minimum that prevent you from rewriting it (you just have to give it a Logger and a Resolver)

--- core/kevoree-commons/lib/Bootstrapper.js
+++ core/kevoree-commons/lib/Bootstrapper.js
@@ -9,6 +9,24 @@ var Bootstrapper = Class({
 
     /**
      *
+     * @param {KevoreeLogger} logger
+     * @param {Resolver} resolver
+     */
+    construct: function (logger, resolver) {
+        if (logger) {
+            this.log = logger;
+            if (resolver) {
+                this.resolver = resolver;
+            } else {
+                throw new Error('No resolver given to '+this.toString()+' (you need to give a proper Resolver to your Bootstrapper)');
+            }
+        } else {
+            throw new Error('No logger given to '+this.toString()+' (you need to give a proper KevoreeLogger to your Bootstrapper)');
+        }
+    },
+
+    /**
+     *
      * @param nodeName
      * @param model
      * @param callback
@@ -35,14 +53,44 @@ var Bootstrapper = Class({
      * @param forceInstall [optional] boolean to indicate whether or not we should force re-installation
      * @param callback                function(Error, Clazz, ContainerRoot)
      */
-    bootstrap: function (deployUnit, forceInstall, callback) {},
+    bootstrap: function (deployUnit, forceInstall, callback) {
+        if (!callback) {
+            // "forceInstall" parameter is not specified (optional)
+            callback = forceInstall;
+            forceInstall = false;
+        }
+
+        // --- Resolvers callback
+        var bootstrapper = this;
+        this.resolver.resolve(deployUnit, forceInstall, function (err, EntityClass, model) {
+            if (err) {
+                bootstrapper.log.error(bootstrapper.toString(), err.stack);
+                return callback(new Error("'"+deployUnit.name+"' bootstrap failed!"));
+            }
+
+            // install success
+            callback(null, EntityClass, model);
+        });
+    },
 
     /**
      *
      * @param deployUnit
      * @param callback
      */
-    uninstall: function (deployUnit, callback) {}
+    uninstall: function (deployUnit, callback) {
+        var bootstrapper = this;
+        this.resolver.uninstall(deployUnit, function (err) {
+            if (err) {
+                bootstrapper.log.error(bootstrapper.toString(), err.stack);
+                callback(new Error("'"+deployUnit.name+"' uninstall failed!"));
+                return;
+            }
+
+            // uninstall success
+            callback(null);
+        });
+    }
 });
 
 module.exports = Bootstrapper;
