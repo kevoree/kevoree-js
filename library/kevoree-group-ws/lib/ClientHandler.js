4c1b0da7538093575f677fe16b28af055a9062c0
Maxime Tricoire <max.tricoire@gmail.com>
2014-10-09 10:49:01 +0200

    kevoree-group-ws: v5.1.1-SNAPSHOT (first release)

---
+++ library/kevoree-group-ws/lib/ClientHandler.js
@@ -0,0 +1,144 @@
+// Created by leiko on 02/10/14 12:20
+var Class = require('pseudoclass');
+var WebSocket = require('ws');
+var kevoree = require('kevoree-library').org.kevoree;
+
+var Protocol = require('./Protocol');
+var PushMessage = require('./message/PushMessage');
+var PullMessage = require('./message/PullMessage');
+
+/**
+ * Handles client events on the WebSocket server
+ *  - connection
+ *  - messages
+ *  - disconnection
+ * @type {ClientHandler}
+ */
+var ClientHandler = Class({
+    toString: 'ClientHandler',
+
+    construct: function (group) {
+        this.group = group;
+        // cache maps
+        this.name2Ws = {};
+        this.ws2Name = {};
+    },
+
+    /**
+     * Returns a client handler for WebSocketServer
+     * @returns {function(this:ClientHandler)}
+     */
+    getHandler: function () {
+        return function (ws) {
+            var wsId = ws.upgradeReq.headers['sec-websocket-key'];
+
+            // kevoree tools
+            var factory = new kevoree.factory.DefaultKevoreeFactory(),
+                loader  = factory.createJSONLoader(),
+                saver   = factory.createJSONSerializer(),
+                compare = factory.createModelCompare();
+
+            // websocket listeners
+            ws.on('close', function () {
+                if (this.ws2Name[wsId] !== null) {
+                    delete this.name2Ws[this.ws2Name[wsId]];
+                }
+                delete this.ws2Name[wsId];
+            }.bind(this));
+
+            ws.on('error', function () {
+                try {
+                    ws.close();
+                } catch (err) {
+                    this.group.log.error(this.group.toString(), err.stack);
+                }
+
+                if (this.ws2Name !== null) {
+                    if (this.ws2Name[wsId] !== null) {
+                        delete this.name2Ws[this.ws2Name[wsId]];
+                    }
+                    delete this.ws2Name[wsId];
+                }
+            }.bind(this));
+
+            ws.on('message', function (msg) {
+                var parsedMsg = Protocol.parse(msg);
+                if (parsedMsg === null) {
+                    this.group.log.error(this.group.toString(), '"'+this.group.getName()+'" unknown Kevoree message '+msg);
+                } else {
+                    switch (parsedMsg.getType()) {
+                        case Protocol.REGISTER_TYPE:
+                            if (!this.name2Ws[parsedMsg.getNodeName()]) {
+                                // cache new client
+                                this.name2Ws[parsedMsg.getNodeName()] = ws;
+                                this.ws2Name[wsId] = parsedMsg.getNodeName();
+
+                                if (this.group.isMaster()) {
+                                    if (!parsedMsg.getModel() || parsedMsg.getModel() === 'null') {
+                                        var currentModel = saver.serialize(this.group.getKevoreeCore().getCurrentModel());
+                                        var pushMessage = new PushMessage(currentModel);
+                                        this.group.log.info(this.group.toString(), 'Sending my model to client "'+parsedMsg.getNodeName()+'"');
+                                        ws.send(pushMessage.toRaw());
+                                    } else {
+                                        // ok i've to merge locally
+                                        var recModel = loader.loadModelFromString(parsedMsg.getModel()).get(0);
+                                        compare.merge(this.group.getKevoreeCore().getCurrentModel(), recModel).applyOn(recModel);
+                                        this.group.log.info(this.group.toString(), 'New client registered "'+parsedMsg.getNodeName()+'". Merging his model with mine');
+                                        this.group.getKevoreeCore().deploy(recModel);
+                                    }
+                                }
+                            }
+                            break;
+
+                        case Protocol.PULL_TYPE:
+                            var modelReturn = saver.serialize(this.group.getKevoreeCore().getCurrentModel());
+                            ws.send(modelReturn);
+                            this.group.log.info(this.group.toString(), 'Pull requested');
+                            break;
+
+                        case Protocol.PUSH_TYPE:
+                            var model = loader.loadModelFromString(parsedMsg.getModel()).get(0);
+                            if (this.group.hasMaster()) {
+                                if (this.group.isMaster()) {
+                                    var count = 0;
+                                    for (var clientName in this.name2Ws) {
+                                        if (this.name2Ws.hasOwnProperty(clientName)) {
+                                            count++;
+                                            if (this.name2Ws[clientName].readyState === WebSocket.OPEN) {
+                                                this.name2Ws[clientName].send(parsedMsg.getModel());
+                                            }
+                                        }
+                                    }
+                                    if (count > 0) {
+                                        this.group.log.info(this.group.toString(), 'Broadcast model over '+count+' client'+((count > 1) ? 's':''));
+                                    }
+                                }
+                            } else {
+                                // TODO ?
+                                this.group.log.info(this.group.toString(), 'No master specified, model will NOT be send to all other nodes');
+                            }
+
+                            this.group.log.info(this.group.toString(), 'Push received, applying locally...');
+                            this.group.getKevoreeCore().deploy(model);
+
+                            break;
+
+                        default:
+                            this.group.log.error(this.group.toString(), '"'+this.group.getName()+'" unhandled Kevoree message ' + msg);
+                            break;
+                    }
+                }
+            }.bind(this));
+        }.bind(this);
+    },
+
+    /**
+     * Clear server caches
+     */
+    clearCache: function () {
+        this.name2Ws = {};
+        this.ws2Name = {};
+    }
+});
+
+module.exports = ClientHandler;
---
+++ library/kevoree-group-ws/lib/WSGroup.js
@@ -0,0 +1,188 @@
+// Created by leiko on 02/10/14 11:47
+var AbstractGroup = require('kevoree-entities').AbstractGroup;
+var kevoree = require('kevoree-library').org.kevoree;
+var WebSocket = require('ws');
+var WebSocketServer = require('ws').Server;
+var SmartSocket = require('smart-socket');
+var ClientHandler = require('./ClientHandler');
+var Protocol = require('./Protocol');
+var PushMessage = require('./message/PushMessage');
+var RegisterMessage = require('./message/RegisterMessage');
+
+/**
+ * Kevoree group
+ */
+var WSGroup = AbstractGroup.extend({
+    toString: 'WSGroup',
+
+    dic_master: { },
+    dic_port:   { fragmentDependant: true, defaultValue: 9000 },
+
+    construct: function () {
+        this.server = null;
+        this.client = null;
+        this.clientHandler = new ClientHandler(this);
+        this.count = 0;
+    },
+
+    start: function (done) {
+        this._super(function () {
+            if (this.hasMaster()) {
+                if (this.isMaster()) {
+                    this.createServer();
+                } else {
+                    this.createClient();
+                }
+            } else {
+                this.createServer();
+            }
+            done();
+        }.bind(this));
+        this.count++;
+    },
+
+    stop: function (done) {
+        this._super(function () {
+            // clear server
+            try {
+                this.server.close();
+            } catch (ignore) {
+                /* prevent not running server to throw err */
+            } finally {
+                this.server = null;
+            }
+
+            // clear cache
+            this.clientHandler.clearCache();
+
+            // clear client
+            if (this.smartSocket) {
+                this.smartSocket.close(true);
+            }
+            this.client = null;
+            done();
+        }.bind(this));
+    },
+
+    update: function (done) {
+        this._super(function () {
+            this.stop(function () {
+                this.start(done);
+            }.bind(this));
+        }.bind(this));
+    },
+
+    createServer: function () {
+        var port = this.dictionary.getNumber('port', 9000);
+        this.server = new WebSocketServer({port: port});
+        this.log.info(this.toString(), '"'+this.getName()+'" listen on ' + port);
+
+        this.server.on('connection', this.clientHandler.getHandler());
+    },
+
+    createClient: function () {
+        if (!this.isMaster()) {
+            if (this.client === null || this.client.readyState === WebSocket.OPEN) {
+                var selfGroup = this.getModelEntity();
+                // localize master node
+                var master = this.dictionary.getString('master');
+                if (selfGroup && master && master.length > 0) {
+                    var masterDico = selfGroup.findFragmentDictionaryByID(master);
+                    var port = this.dic_port.defaultValue;
+                    if (masterDico) {
+                        var val = masterDico.findValuesByID('port');
+                        port = val.value;
+                    }
+                    var addresses = [];
+                    var node = this.getKevoreeCore().getDeployModel().findNodesByID(master);
+                    if (node) {
+                        node.networkInformation.array.forEach(function (net) {
+                            net.values.array.forEach(function (prop) {
+                                if (net.name.toLowerCase().indexOf('ip') !== -1 || prop.name.toLowerCase().indexOf('ip') !== -1) {
+                                    if (prop.value.indexOf(':') !== -1) {
+                                        addresses.push('[' + prop.value + ']' + ':' + port);
+                                    } else {
+                                        addresses.push(prop.value + ':' + port);
+                                    }
+                                }
+                            });
+                        });
+
+                        if (addresses.length === 0) {
+                            throw new Error('WSGroup error: "'+this.getName()+'" unable to find a suitable address for master server "'+master+'"');
+                        }
+
+                        var factory = new kevoree.factory.DefaultKevoreeFactory();
+                        var loader = factory.createJSONLoader();
+                        var saver = factory.createJSONSerializer();
+
+                        this.smartSocket = new SmartSocket({
+                            addresses: addresses,
+                            loopBreak: 3000
+                        });
+
+                        this.smartSocket.start();
+
+                        this.smartSocket.on('open', function (ws) {
+                            this.log.info(this.toString(), '"' + this.getName() + '" connected to master server ' + ws.url);
+                            this.client = ws;
+
+                            var currentModel = saver.serialize(this.getKevoreeCore().getCurrentModel());
+                            var registerMsg = new RegisterMessage(this.getNodeName(), currentModel);
+                            ws.send(registerMsg.toRaw());
+                        }.bind(this));
+
+                        this.smartSocket.on('close', function (ws) {
+                            this.log.info(this.toString(), 'Lost connection with master "'+master+'" at '+ws.url);
+                        }.bind(this));
+
+                        this.smartSocket.on('message', function (ws, msg) {
+                            var parsedMsg = Protocol.parse(msg);
+                            if (!parsedMsg) {
+                                this.log.error(this.toString(), '"'+this.getName()+'" unknown Kevoree message ' + message);
+                            } else {
+                                switch (parsedMsg.getType()) {
+                                    case Protocol.PUSH_TYPE:
+                                        // push from master
+                                        var model = loader.loadModelFromString(parsedMsg.getModel()).get(0);
+                                        this.getKevoreeCore().deploy(model);
+                                        break;
+
+                                    default:
+                                        this.log.error(this.toString(), '"'+this.getName()+'" unhandled Kevoree message ' + message);
+                                        break;
+                                }
+                            }
+                        }.bind(this));
+                    }
+                }
+            }
+        }
+    },
+
+    pushToMaster: function (serializedModel) {
+        if (this.client != null && this.client.readyState === WebSocket.OPEN) {
+            var pushMessage = new PushMessage(serializedModel);
+            this.client.send(pushMessage.toRaw());
+            return true;
+        } else {
+            return false;
+        }
+    },
+
+    hasMaster: function () {
+        var master = this.dictionary.getString('master');
+        return (master && master.length > 0);
+    },
+
+    isMaster: function () {
+        var master = this.dictionary.getString('master');
+        if (this.hasMaster()) {
+            return master === this.getNodeName();
+        } else {
+            return false;
+        }
+    }
+});
+
+module.exports = WSGroup;
