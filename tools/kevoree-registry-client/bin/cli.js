e780847a401f86560bc3c41987ea83387d74c3bc
Maxime Tricoire <max.tricoire@gmail.com>
2014-10-28 15:20:20 +0100

    kevoree-registry-client: fix wrong commit

--- tools/kevoree-registry-client/bin/cli.js
+++ tools/kevoree-registry-client/bin/cli.js
@@ -13,4 +13,36 @@ var argv = minimist(process.argv.slice(2), {
     string:  ['o', 'host', 'port']
 });
 
-console.log(argv);
+function cmdHandler(err) {
+    if (err) {
+        process.stderr.write(chalk.red('Error')+' '+err.message+'\n');
+        if (argv.verbose) {
+            err.stack = err.stack.split('\n');
+            err.stack.splice(0, 1);
+            err.stack = err.stack.join('\n');
+            process.stderr.write(err.stack+'\n');
+        }
+        process.exit(1);
+    } else {
+        process.exit(0);
+    }
+}
+
+var help = require('../lib/commands/help');
+var version = require('../lib/commands/version');
+
+try {
+    var command = require('../lib/commands/'+argv._[0]);
+    command(argv, cmdHandler);
+} catch (err) {
+    if (err.code === 'MODULE_NOT_FOUND') {
+        if (argv.v) {
+            version(argv, cmdHandler);
+        } else {
+            help(argv, cmdHandler);
+        }
+    } else {
+        cmdHandler(err);
+        process.exit(1);
+    }
+}
