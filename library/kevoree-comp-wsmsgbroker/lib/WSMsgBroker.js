caeaf25219762d9e3f6cd100cd9849e8fe662bcf
Maxime Tricoire <max.tricoire@gmail.com>
2014-10-30 12:09:38 +0100

    kevoree-comp-wsmsgbroker: v1.0.0 using external dep 'wsmsgbroker'

--- library/kevoree-comp-wsmsgbroker/lib/WSMsgBroker.js
+++ library/kevoree-comp-wsmsgbroker/lib/WSMsgBroker.js
@@ -1,5 +1,5 @@
 var AbstractComponent = require('kevoree-entities').AbstractComponent;
-var WebSocket = require('ws');
+var WSBroker = require('wsmsgbroker');
 var chalk = require('chalk');
 
 /**
@@ -12,11 +12,6 @@ var WSMsgBroker = AbstractComponent.extend({
     dic_port: { optional: false, datatype: 'number' },
     dic_path: { },
 
-    construct: function () {
-        this.id2ws = {};
-        this.ws2id = {};
-    },
-
     /**
      * this method will be called by the Kevoree platform when your component has to start
      * @param {Function} done
@@ -26,42 +21,24 @@ var WSMsgBroker = AbstractComponent.extend({
             var port = this.dictionary.getNumber('port');
             if (port) {
                 var path = this.dictionary.getString('path', '');
-                if (path.substr(0, 1) !== '/') {
-                    path = '/' + path;
-                }
 
-                this.server = new WebSocket.Server({ port: port, path: path });
+                // create a new WSMsgBroker server
+                this.server = new WSBroker.Server(port, path);
                 this.log.info(this.toString(), '"'+this.getName()+'" listen on 0.0.0.0:'+port+path);
 
-                this.server.on('connection', function (ws) {
-                    ws.on('message', function (msg) {
-                        if (typeof msg === 'object') { msg = msg.data; }
-
-                        try {
-                            msg = JSON.parse(msg);
-                            var action = require('./actions/'+msg.action);
-                            if (action) {
-                                try {
-                                    action(this)(ws, msg);
-                                } catch (err) {
-                                    this.log.error(this.toString(), '"'+this.getName()+'" '+err.message);
-                                }
-                            } else {
-                                this.log.error(this.toString(), '"'+this.getName()+'" malformed action "'+msg.action+'"');
-                            }
-                        } catch (err) {
-                            if (err.code === 'MODULE_NOT_FOUND') {
-                                this.log.warn(this.toString(), '"'+this.getName()+'" unknown action "'+msg.action+'"');
-                            } else {
-                                this.log.error(this.toString(), '"'+this.getName()+'" unable to parse message (only support JSON-encoded messages)');
-                            }
-                        }
-                    }.bind(this));
-
-                    ws.on('close', function () {
-                        this.unregister(ws);
-                    }.bind(this));
+                // register some event listeners on it
+                this.server.on('error', function (err) {
+                    this.log.warn(this.toString(), '"'+this.getName()+'" broker error: '+err.message);
                 }.bind(this));
+
+                this.server.on('registered', function (id) {
+                    this.log.info(this.toString(), '"'+this.getName()+'" '+chalk.green('+')+' '+id);
+                }.bind(this));
+
+                this.server.on('unregistered', function (id) {
+                    this.log.info(this.toString(), '"'+this.getName()+'" '+chalk.yellow('-')+' '+id);
+                }.bind(this));
+
                 done();
 
             } else {
@@ -76,18 +53,7 @@ var WSMsgBroker = AbstractComponent.extend({
      */
     stop: function (done) {
         this._super(function () {
-
-            try {
-                this.server.close();
-            } catch (ignore) {
-                // no one care if this fails
-            } finally {
-                this.server = null;
-            }
-
-            this.id2ws = {};
-            this.ws2id = {};
-
+            this.server.close();
             done();
         }.bind(this));
     },
@@ -98,37 +64,6 @@ var WSMsgBroker = AbstractComponent.extend({
                 this.start(done);
             }.bind(this));
         }.bind(this));
-    },
-
-    send: function (id, message) {
-        var ws = this.id2ws[id];
-        if (ws && ws.readyState === WebSocket.OPEN) {
-            ws.send(message);
-        }
-    },
-
-    /**
-     * Register a WebSocket client on the server
-     * @param {String} id
-     * @param {WebSocket} ws
-     */
-    register: function (id, ws) {
-        this.id2ws[id] = ws;
-        this.ws2id[ws] = id;
-        this.log.info(this.toString(), '"'+this.getName()+'" '+chalk.green('+')+' '+id);
-    },
-
-    /**
-     * Unregister a WebSocket client from server
-     * @param {WebSocket} ws
-     */
-    unregister: function (ws) {
-        var id = this.ws2id[ws];
-        if (id) {
-            delete this.ws2id[ws];
-            delete this.id2ws[id];
-            this.log.info(this.toString(), '"'+this.getName()+'" '+chalk.yellow('-')+' '+id);
-        }
     }
 });
 
