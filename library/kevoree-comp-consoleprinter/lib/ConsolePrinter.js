8ae244e114b5e436fba294a6dbe809b60cd7de6e
Maxime Tricoire <max.tricoire@gmail.com>
2015-03-20 18:21:28 +0100

    kevoree-comp-consoleprinter: v5.2.6-SNAPSHOT new browser runtime ready

--- library/kevoree-comp-consoleprinter/lib/ConsolePrinter.js
+++ library/kevoree-comp-consoleprinter/lib/ConsolePrinter.js
@@ -7,8 +7,26 @@ var AbstractComponent = require('kevoree-entities').AbstractComponent;
 var ConsolePrinter = AbstractComponent.extend({
     toString: 'ConsolePrinter',
 
+    construct: function () {
+        this.lines = [];
+    },
+
     in_input: function (msg) {
-        console.log(this.getName() + '>' + msg);
+        var line = this.getName() + '>' + msg;
+        this.lines.push(line);
+        console.log(line);
+    },
+
+    uiController: function () {
+        var that = this;
+        return ['$scope', '$interval', function ($scope, $interval) {
+            $scope.name = that.getName();
+            $scope.lines = that.lines;
+
+            $interval(function () {
+                $scope.lines = that.lines;
+            }, 1000);
+        }];
     }
 });
 
