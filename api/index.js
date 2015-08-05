4492983e4a46c7c26b504f63254d30e86f8f426d
Maxime Tricoire <max.tricoire@gmail.com>
2015-08-05 15:32:57 +0200

    clean

--- api/index.js
+++
@@ -1,7 +0,0 @@
-// Kevoree Injector
-exports.Kevoree        = require('./lib/Kevoree');
-
-// Kevoree Abstract Types
-exports.TypeDefinition = require('./lib/tdefs/TypeDefinition');
-exports.Component      = require('./lib/tdefs/Component');
-exports.Node           = require('./lib/tdefs/Node');
