96ea2fba8bef1004a48e3974e8797cbf10ebf4b4
Maxime Tricoire <max.tricoire@gmail.com>
2015-11-13 15:30:12 +0100

    update model

--- model/src/test/test.ts
+++ model/src/test/test.ts
@@ -1,27 +1,23 @@
-/**
 import { org } from '../main/model';
 
 describe('Kevoree model tests', () => {
-  var dm = org.kevoree.modeling.memory.manager.DataManagerBuilder.buildDefault();
-  var kModel = new org.KevoreeModel(dm);
+    var dm = org.kevoree.modeling.memory.manager.DataManagerBuilder.buildDefault();
+    var kModel = new org.KevoreeModel(dm);
 
-  kModel.connect(() => {
-    var kView = kModel.universe(0).time(0);
-    var model = kView.createModel();
-    var listener = kModel.createListener(0);
-    listener.listen(model);
-    listener.then((o: org.kevoree.NamedElement) => {
-      console.log('update', o.getName());
-    });
+    kModel.connect(() => {
+        var kView = kModel.universe(0).time(0);
+        var model = kView.createModel();
+        var listener = kModel.createListener(0);
+        listener.listen(model);
+        listener.then((o: org.kevoree.Element) => {
+            console.log('Updated', o.toJSON());
+        });
 
-    it('add node', (done: MochaDone) => {
-      var node = kView.createNode();
-      node.setName('myNode');
-      model.addNodes(node);
-      done();
-    });
-
-
-  })
-})
-**/
+        it('add node', (done: MochaDone) => {
+            var node = kView.createNode();
+            node.setName('myNode');
+            model.addNodes(node);
+            done();
+        });
+    })
+});
