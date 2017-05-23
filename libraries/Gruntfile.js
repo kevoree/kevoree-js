5df876628cd68e6b9bdec1fd5b498265ad8ab5b2
Maxime Tricoire <max.tricoire@gmail.com>
2017-05-23 18:54:56 +0200

    towards new registry

--- libraries/Gruntfile.js
+++ libraries/Gruntfile.js
@@ -4,52 +4,43 @@ const PROJECTS = [
 	'chan/{local,remotews}',
 	'comp/{consoleprinter,ticker,mqtt*}',
 	'node/*',
-	'group/{ws,remotews}'
+	'group/{ws,remotews,centralizedws}'
 ];
 
 module.exports = function (grunt) {
-	require('time-grunt')(grunt);
-	require('./grunt-exec')(grunt);
+  require('time-grunt')(grunt);
+  require('./grunt-exec')(grunt);
 
-	grunt.initConfig({
-		exec: {
-			clean: {
-				src: PROJECTS,
-				options: {
-					cmd: 'rm',
-					args: ['-rf', 'node_modules']
-				}
-			},
-			install: {
-				src: PROJECTS,
-				options: {
-					cmd: 'npm',
-					args: ['install']
-				}
-			},
-			grunt: {
-				src: PROJECTS,
-				options: {
-					cmd: 'grunt',
-					args: ['publish']
-				}
-			},
-			git: {
-				src: PROJECTS,
-				options: {
-					cmd: 'git',
-					args: ['push']
-				}
-			},
-			publish: {
-				src: PROJECTS,
-				options: {
-					cmd: 'npm',
-					args: ['publish']
-				}
-			}
-		}
-	});
+  grunt.initConfig({
+    exec: {
+      custom: {
+        // use this to execute custom commands directly from shell
+        // eg. grunt exec:custom --cmd=ls --args=kev --args=node_modules
+        src: PROJECTS
+      },
+      clean: {
+        src: PROJECTS,
+        options: {
+          cmd: 'rm',
+          args: ['-rf', 'node_modules']
+        }
+      },
+      install: {
+        src: PROJECTS,
+        options: {
+          cmd: 'npm',
+          args: ['install']
+        }
+      },
+      publish: {
+        src: PROJECTS,
+        options: {
+          cmd: 'grunt',
+          args: ['publish']
+        }
+      }
+    }
+  });
 
-	grunt.registerTask('default', 'exec');
+  grunt.registerTask('default', 'exec');
 };
--- libraries/grunt-exec.js
+++ libraries/grunt-exec.js
@@ -6,73 +6,88 @@ const chalk = require('chalk');
 const os = require('os');
 const ora = require('ora');
 const readline = require('readline');
+const qs = require('querystring');
 
 module.exports = function (grunt) {
-	const spinner = ora();
+  const spinner = ora();
 
-	function log(txt) {
-		spinner.text = txt;
-	}
+  function log(txt) {
+    spinner.text = txt;
+  }
 
-	function newLog(txt) {
-		readline.clearLine(process.stdout);
-		readline.cursorTo(process.stdout, 0);
-		grunt.log.writeln(txt);
-	}
+  function newLog(txt) {
+    readline.clearLine(process.stdout);
+    readline.cursorTo(process.stdout, 0);
+    grunt.log.writeln(txt);
+  }
 
-	grunt.registerMultiTask('exec', 'Exec the given command in the specified src directories', function () {
-		const done = this.async();
+  grunt.registerMultiTask('exec', 'Exec the given command in the specified src directories', function () {
+    const done = this.async();
 
-		const options = this.options({
-			cmd: null,
-			args: []
-		});
+    const options = this.options({
+      cmd: null,
+      args: []
+    });
 
-		if (!options.cmd || !options.cmd.trim().length) {
-			grunt.fail.fatal(new Error('options.cmd must be set (current: ' + options.cmd + ')'));
-		} else {
-			let cmd = options.cmd;
-			const args = options.args;
-			if (/^win/.test(process.platform)) {
-				cmd = process.env.comspec;
-				args.unshift(options.cmd);
-				args.unshift('/c');
-			}
+    if (grunt.option('cmd')) {
+      options.cmd = grunt.option('cmd');
+    }
 
-			grunt.log.ok('Executing: ' + chalk.gray(options.cmd + ' ' + args.join(' ')));
+    if (grunt.option('args')) {
+      options.args = [].concat(grunt.option('args'));
+    }
 
-			spinner.start();
-			async.parallelLimit(this.filesSrc.filter(function (path) {
-				return grunt.file.isDir(path);
-			}).map(function (projectPath) {
-				return function (cb) {
-					let relPath = path.relative(__dirname, projectPath);
-					log(chalk.cyan(relPath) + ' running...');
-					grunt.util.spawn({
-						cmd: cmd,
-						args: args,
-						opts: {
-							cwd: projectPath,
-							stdio: ['ignore', 'pipe', 'pipe']
-						}
-					}, function (err, result) {
-						if (err) {
-							const stdLogs = `.exec-logs/${options.cmd}/${args.join('_')}/${relPath.replace(/\//g, '-')}`;
-							newLog(chalk.red('✘') + ' ' + chalk.cyan(relPath) + ' ' + chalk.grey('(' + stdLogs + ')'));
-							const now = Date.now();
-							grunt.file.write(`${stdLogs}/stdout.${now}.log`, result.stdout);
-							grunt.file.write(`${stdLogs}/stderr.${now}.log`, result.stderr);
-							cb();
-						} else {
-							newLog(chalk.green('✔') + ' ' + chalk.cyan(relPath));
-							cb();
-						}
-					});
-				};
-			}), os.cpus().length, function (err) {
-				spinner.stop();
-				done(err);
-			});
-		}
-	});
+    if (!options.cmd || !options.cmd.trim().length) {
+      grunt.fail.fatal(new Error('options.cmd must be set (current: ' + options.cmd + ')'));
+    } else {
+      let cmd = options.cmd;
+      const args = options.args;
+      if (/^win/.test(process.platform)) {
+        cmd = process.env.comspec;
+        args.unshift(options.cmd);
+        args.unshift('/c');
+      }
+
+      grunt.log.ok('Executing: ' + chalk.gray(options.cmd + ' ' + args.join(' ')));
+
+      spinner.start();
+      async.parallelLimit(this.filesSrc.filter(function (path) {
+        return grunt.file.isDir(path);
+      }).map(function (projectPath) {
+        return function (cb) {
+          let relPath = path.relative(__dirname, projectPath);
+          log(chalk.cyan(relPath) + ' running...');
+          grunt.util.spawn({
+            cmd: cmd,
+            args: args,
+            opts: {
+              cwd: projectPath,
+              stdio: ['ignore', 'pipe', 'pipe']
+            }
+          }, function (err, result) {
+            const escapedArgs = args.map(function (arg) {
+              return qs.escape(arg);
+            });
+            const now = new Date().toISOString();
+            const stdLogs = `.exec-logs/${options.cmd}/${escapedArgs.join('_')}/${relPath.replace(/\//g, '-')}`;
+            if (err) {
+              newLog(chalk.red('✘') + ' ' + chalk.cyan(relPath) + ' ' + chalk.grey('(' + stdLogs + ')'));
+            } else {
+              newLog(chalk.green('✔') + ' ' + chalk.cyan(relPath));
+            }
+            if (result.stdout) {
+              grunt.file.write(`${stdLogs}/stdout.${now}.log`, result.stdout);
+            }
+            if (result.stderr) {
+              grunt.file.write(`${stdLogs}/stderr.${now}.log`, result.stderr);
+            }
+            cb();
+          });
+        };
+      }), os.cpus().length, function (err) {
+        spinner.stop();
+        done(err);
+      });
+    }
+  });
 };
